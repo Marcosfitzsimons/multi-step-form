a71bc96f2c7ea415d96d95f415c723e1529d5267
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-02-17 00:38:15 -0300

    Add next font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,12 @@
+import { Inter } from "@next/font/google";
+
 import "../styles/globals.css";
+
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -6,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const { fontFamily } = require("tailwindcss/defaultTheme")
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class", '[data-theme="dark"]'],
@@ -7,7 +5,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ['var(--font-inter)'],
       },
       keyframes: {
         "accordion-down": {
