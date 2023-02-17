a75fc54a0b1a7393c2102692726be0e66c8ea4a1
marcosfitzsimons <marcosfitzsimons@gmail.com>
2023-02-17 17:05:08 -0300

    Add styles to form wrapper

--- components/FormWrapper.tsx
+++ components/FormWrapper.tsx
@@ -1,19 +1,21 @@
-import { ReactNode } from "react"
+import { ReactNode } from "react";
 
 type FormWrapperProps = {
-    title: string
-    description: string
-    children: ReactNode
-}
+  title: string;
+  description: string;
+  children: ReactNode;
+};
 
 const FormWrapper = ({ title, description, children }: FormWrapperProps) => {
   return (
-    <div>
-        <h2>{title}</h2>
-        <p>{description}</p>
-        {children}
+    <div className="flex flex-col gap-5">
+      <div className="flex flex-col gap-1">
+        <h2 className="text-2xl font-semibold text-white">{title}</h2>
+        <p className="text-neutral-300">{description}</p>
+      </div>
+      {children}
     </div>
-  )
-}
+  );
+};
 
-export default FormWrapper
+export default FormWrapper;
